4a59d219a1db7e5d42cf3d74c9225263304c61ee
hadisyahrial <hadisyahrial@gmail.com>
2026-02-27 07:51:27 +0700

    update api url to railway

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://todo-backend-production-b298.up.railway.app/api'
 });
 
 export const getTodos = () => API.get('/todos');
